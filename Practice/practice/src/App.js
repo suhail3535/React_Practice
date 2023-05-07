9c4de8d09e9afc36bf0983d3ae865a65cc399363
suhail3535 <khansohail015@gmail.com>
2023-05-08 00:17:28 +0530

    rendom practice

--- Practice/practice/src/App.js
+++ Practice/practice/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Product from './Component/Actions/Action';
-import Page from './Component/View/View';
-import Action from './Component/Actions/Action';
 
-function App() {
+
+function App () {
+
+
+
   return (
       <div className="App">
-          <Action />
-          <Page />
+
       </div>
   );
 }
