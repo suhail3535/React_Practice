7aaaad359ba583398384e143f44d0ec4fb8f8fd6
suhail3535 <khansohail015@gmail.com>
2023-05-17 22:44:11 +0530

    newkeybard

--- Practice/mypractice/src/cpmponents/indexx.js
+++ Practice/mypractice/src/cpmponents/indexx.js
@@ -44,18 +44,61 @@
 
 // sayHello();
 
+// var john = {
+//     name: "John Doe",
+//     age: 30,
+//     sayName: function () {
+//         // this.age = 35;
+//         console.log("My name is ", this.name);
+//     },
+//     sayHello: function() {
+//         console.log("Hello! from ", this.name);
+//     },
+// };
 
-var john = {
-    name: "John Doe",
-    age: 30,
-    sayName: function () {
-        // this.age = 35;
-        console.log("My name is ", this.name);
-    },
-    sayHello: function() {
-        console.log("Hello! from ", this.name);
-    },
-};
+// john.sayName();
+// john.sayHello();
 
-john.sayName();
+// function sayHello() {
+//     console.log("Hello! ", this);
+// }
+
+// var john = {
+//     name: "John Doe",
+//     age: 30,
+// };
+
+// var james = {
+//     name: "James Bond",
+//     age: 30,
+// };
+
+// sayHello.call(james);
+// sayHello.apply(john);
+// function sayHello() {
+//     console.log("Hello! ", this, this.name);
+// }
+
+// var John = {
+//     name: "John Doe",
+//     age: 30,
+// };
+
+// var johnSayHello = sayHello.bind(John);
+
+// johnSayHello();
+
+// When a function is called with the new operator, it gives us a brand new object & the this refers to the newly created object.
+function Person(name) {
+    this.name = name;
+
+    this.sayHello = function () {
+        console.log("Hello! ", this, this.name);
+    };
+}
+
+var james = new Person("James Bond");
+var john = new Person("John Doe");
+
 john.sayHello();
+james.sayHello();
